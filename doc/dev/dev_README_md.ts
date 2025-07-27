450db3e0ea39cc9e44a5c1a6640c53fa76b63149
hunt <hunt.bao@outlook.com>
2025-07-27 11:18:24 +0800

    更新文档

--- doc/dev/dev_README_md.ts
+++ doc/dev/dev_README_md.ts
@@ -1,38 +1,3 @@
-# 数据库
-
-```
-// ------- 本地 -------
-// 查看所有表
-npx wrangler d1 execute DB --command "SELECT name FROM sqlite_master WHERE type='table';"
-
-// 增加0003_add_feedback_to_messages.sql
-npx wrangler d1 execute DB --file ./src/sql/0003_add_feedback_to_messages.sql --local
-
-// 删除ChatMessage和Chat表，并重新创建
-cd nextjs ; npx  wrangler d1 execute DB --command "DROP TABLE IF EXISTS ChatMessage; DROP TABLE IF EXISTS Chat;" ; npx wrangler d1 execute DB --file ./src/sql/0001_create_messages_table.sql ; npx  wrangler d1 execute DB --file ./src/sql/0002_create_chat_sessions_table.sql
-
-// 测试数据库 
-npx wrangler d1 execute test_d1 --local --file=./src/app/test/d1/schema.sql
-npx wrangler d1 execute test_d1 --local --command="SELECT * FROM Customers"
-
-// 本地，查看所有表
-npx wrangler d1 execute test_d1 --local --command="SELECT name FROM sqlite_master WHERE type='table';"
-// 查看Chat表
-npx wrangler d1 execute test_d1 --local --command="SELECT * FROM Chat"     
-// 查看ChatMessage表
-npx wrangler d1 execute test_d1 --local --command="SELECT * FROM ChatMessage"
-
-// ------- 远程 -------
-// 增加0003_add_feedback_to_messages.sql
-npx wrangler d1 execute DB --file ./src/sql/0003_add_feedback_to_messages.sql --remote
-
-// 删除ChatMessage和Chat表，并重新创建
-cd nextjs ; npx  wrangler d1 execute DB  --remote --command "DROP TABLE IF EXISTS ChatMessage; DROP TABLE IF EXISTS Chat;" ; npx wrangler d1 execute DB --remote --file ./src/sql/0001_create_messages_table.sql ; npx  wrangler d1 execute DB --remote --file ./src/sql/0002_create_chat_sessions_table.sql
-
-
-npx wrangler d1 execute test_d1 --remote --file=./src/app/test/d1/schema.sql
-npx wrangler d1 execute test_d1 --remote --command="SELECT * FROM Customers"
-```
 
 # 环境变量配置
 
